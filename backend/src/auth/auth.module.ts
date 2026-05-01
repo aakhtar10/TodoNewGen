0c041071b6b2eee41375d8eeea77731a58a74df5
aakhtar10 <arsalanakhtar124@gmail.com>
2026-05-01 14:53:06 +0530

    ds

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserModule } from 'src/user/user.module';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { UserModule } from '../user/user.module';
+import { PrismaService } from '../prisma/prisma.service';
+
 
 @Module({
   imports:[UserModule],
