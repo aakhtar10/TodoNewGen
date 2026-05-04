acf3a3c1427e21472542ae6fdd66edb18ede638a
aakhtar10 <arsalanakhtar124@gmail.com>
2026-05-04 17:02:23 +0530

    backend fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
  app.enableCors({
-    origin: ["http://localhost:3001","http://localhost:3000","https://todo-new-gen.vercel.app/"], // your Next.js app
+    origin: ["http://localhost:3001","http://localhost:3000","https://todo-new-gen.vercel.app"], // your Next.js app
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   });
